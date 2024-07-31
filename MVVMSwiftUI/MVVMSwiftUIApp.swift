c2a0692516d8d1eb6b88b21eff2460668edb01e2
KDaria <dkdva@mail.ru>
2024-07-31 18:11:59 +0200

    Initial Commit

---
+++ MVVMSwiftUI/MVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIApp.swift
+//  MVVMSwiftUI
+//
+//  Created by Дарья Кобелева on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
